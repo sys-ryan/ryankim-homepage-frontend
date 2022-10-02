78101ce334efc57b12ee1bccf7662d79eeb67098
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-10-03 01:07:23 +0900

    feat: 메인 페이지 3

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,16 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import HomeBoard from "./components/HomeBoard";
-import HomeBoard1 from "./components/HomeBoard1";
+import HomeBoard from "./components/Home/HomeBoard";
+import HomeBoard1 from "./components/Home/HomeBoard1";
+import HomeBoard3 from "./components/Home/HomeBoard3";
 
 const Home: NextPage = () => {
   return (
     <>
       <HomeBoard1 />
       <HomeBoard />
+      <HomeBoard3 />
       <HomeBoard />
     </>
   );
