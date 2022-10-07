23ec02b48d876115329188d1a7e484180a422fc2
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-10-08 02:40:34 +0900

    feat: HomeBoardContacts animation

--- pages/ui/Menu.tsx
+++ pages/ui/Menu.tsx
@@ -1,15 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 
-const Menu = (props: { className: string }) => {
+const Menu = (props: { className: string; isVisible: boolean; clickHandler: any }) => {
   const openClasses = "fixed  lg:grid";
+
+  // const [classes, setClasses] = useState();
+
+  const classes = props.isVisible ? "fixed lg:grid animate-fadeIn animate-duration-800" : "hidden";
+
   return (
-    <div
-      className={`h-screen w-screen bg-black top-0 left-0 z-30 lg:grid-cols-3 ${props.className}`}
-    >
+    <div className={`h-screen w-screen bg-black top-0 left-0 z-30 lg:grid-cols-3 ${classes}`}>
       <div className="h-full w-full bg-black lg:col-span-1 lg:z-10">
         <div className=" text-white h-[60%] flex flex-col justify-between w-[70%] mt-[40%] mx-auto text-4xl font-semibold lg:mt-[30%] lg:h-[60%] ">
-          <div className="flex">
-            <p className="hover:text-primary-orange hover:cursor-pointer">Home</p>
+          <div className="flex" onClick={props.clickHandler}>
+            <p className="hover:text-primary-orange hover:cursor-pointer">
+              <Link href="/">Home</Link>
+            </p>
           </div>
           <div className="flex">
             <p className="hover:text-primary-orange hover:cursor-pointer">Web Backend</p>
