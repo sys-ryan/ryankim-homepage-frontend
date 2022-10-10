6e627fb41932c6c8bb00809f4291903f07a565a8
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-10-11 03:11:28 +0900

    feat: PostContent desktop size `

--- pages/components/Blog/posts/post-detail/PostContent.tsx
+++ pages/components/Blog/posts/post-detail/PostContent.tsx
@@ -1,7 +1,7 @@
 import PostHeader from "./PostHeader";
 
 const DUMMY_POST = {
-  title: "POST Title",
+  title: "POST Title which is very long so that I can test something",
   excerpt: "this is blog post excerpt",
   imagePath:
     "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1120&q=80",
@@ -14,7 +14,7 @@ const PostContent = () => {
 
   return (
     <>
-      <div className="w-[90%] m-auto mt-[5%] lg:mt-[2%]">
+      <div className="w-[90%] m-auto mt-[5%] lg:mt-[2%] lg:w-[60%] lg:max-w-[1000px]">
         <PostHeader title={DUMMY_POST.title} image={DUMMY_POST.imagePath} date={postDate} />
         {DUMMY_POST.content}
       </div>
