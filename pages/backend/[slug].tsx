797cef2507c520c79700e051260c9e746626786c
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-10-11 03:03:40 +0900

    feat: PostDetail

---
+++ pages/backend/[slug].tsx
@@ -0,0 +1,14 @@
+import BlogHeader from "../components/Blog/BlogHeader";
+import BlogMenu from "../components/Blog/BlogMenu";
+import PostContent from "../components/Blog/posts/post-detail/PostContent";
+
+const BackendPost = () => {
+  return (
+    <>
+      <BlogHeader />
+      <BlogMenu />
+      <PostContent />
+    </>
+  );
+};
+export default BackendPost;
