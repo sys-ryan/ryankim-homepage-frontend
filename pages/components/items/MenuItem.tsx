8647c7209a6411f6f480855b60027a985df1425c
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-10-10 23:12:33 +0900

    fix: MenuItem

--- pages/components/items/MenuItem.tsx
+++ pages/components/items/MenuItem.tsx
@@ -4,7 +4,7 @@ const MenuItem = (props: { clickHandler: any; to: string; title: string }) => {
   return (
     <div className="flex" onClick={props.clickHandler}>
       <p className="hover:text-primary-orange hover:cursor-pointer">
-        <Link href={props.to}>{props.title}</Link>
+        <Link href={props.to ?? ""}>{props.title}</Link>
       </p>
     </div>
   );
