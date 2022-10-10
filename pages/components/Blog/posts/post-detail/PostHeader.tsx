b011dba6bdda24832f3c09887457db8ef6b07b5d
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-10-11 03:11:28 +0900

    feat: PostContent desktop size `

--- pages/components/Blog/posts/post-detail/PostHeader.tsx
+++ pages/components/Blog/posts/post-detail/PostHeader.tsx
@@ -8,16 +8,18 @@ const PostHeader = (props: { title: string; image: string; date: Date }) => {
   });
 
   return (
-    <header className="mb-8">
-      <div className="border-b-[1px] border-gray-400 pb-5">
-        <h1 className="font-bold text-3xl pb-3">{props.title}</h1>
-        <Image
-          src={props.image}
-          alt={props.title}
-          width={1600}
-          height={900}
-          className="object-cover "
-        />
+    <header className="mb-8 lg:mb-20">
+      <div className="border-b-[1px] border-gray-400 pb-5 lg:flex lg:items-center">
+        <h1 className="font-bold text-3xl pb-3 lg:text-5xl w-[80%] lg:pr-8">{props.title}</h1>
+        <div className="lg:w-[40%]">
+          <Image
+            src={props.image}
+            alt={props.title}
+            width={1600}
+            height={900}
+            className="object-cover"
+          />
+        </div>
       </div>
       <p className="text-gray-600 mb-3 mt-">{postDate}</p>
     </header>
