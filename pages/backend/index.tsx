873514ef59d859085434a90609ce1b85e1e242cd
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-10-12 19:41:30 +0900

    feat: post item 클릭 시 post detail 이동

--- pages/backend/index.tsx
+++ pages/backend/index.tsx
@@ -1,11 +1,26 @@
 import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
+import { useRouter } from "next/router";
 import { Post } from "../../interfaces/post.interface";
+import BlogHeader from "../components/Blog/BlogHeader";
 import BlogHome from "../components/Blog/BlogHome";
+import BlogMenu from "../components/Blog/BlogMenu";
+import PostList from "../components/Blog/posts/PostList";
 
 const BackendPage: NextPage = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const router = useRouter();
+
+  const asPath = router.asPath;
+  const postBasePath = `${process.env.backendBaseUrl}/${asPath}`;
+
   return (
     <div>
-      <BlogHome posts={props.posts} />
+      {/* <BlogHome posts={props.posts} /> */}
+      <div>
+        <BlogHeader />
+        <BlogMenu />
+
+        <PostList posts={props.posts} postBasePath={postBasePath} />
+      </div>
     </div>
   );
 };
