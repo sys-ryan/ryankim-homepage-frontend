f8cf0e9e828f1477e3f67f5d83980990956a0f09
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-10-12 18:46:40 +0900

    feat: backend/[id] page에 Head Meta 추가

--- pages/backend/[id].tsx
+++ pages/backend/[id].tsx
@@ -5,15 +5,27 @@ import {
   InferGetStaticPropsType,
   NextPage,
 } from "next";
+import { useRouter } from "next/router";
 import { Post } from "../../interfaces/post.interface";
 import { parseMarkdown } from "../../lib/posts-util";
 import BlogHeader from "../components/Blog/BlogHeader";
 import BlogMenu from "../components/Blog/BlogMenu";
 import PostContent from "../components/Blog/posts/post-detail/PostContent";
+import HeadMeta from "../components/HeadMeta";
 
 const BackendPost: NextPage = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const router = useRouter();
+  // TOOD: Loading Spinner
+
+  const subCategory =
+    props.post.subCategory.title.charAt(0).toUpperCase() + props.post.subCategory.title.slice(1);
   return (
     <>
+      <HeadMeta
+        title={`${subCategory} - ${props.post.title}`}
+        description={props.post.excerpt}
+        url={`${process.env.backendBaseUrl}/${router.asPath}`}
+      />
       <BlogHeader />
       <BlogMenu />
       <PostContent post={props.post} content={props.content} />
--- pages/components/Blog/posts/post-detail/PostContent.tsx
+++ pages/components/Blog/posts/post-detail/PostContent.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import { ReactNode } from "react";
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { nightOwl } from "react-syntax-highlighter/dist/cjs/styles/prism";
@@ -7,15 +5,6 @@ import remarkGfm from "remark-gfm";
 import { Post } from "../../../../../interfaces/post.interface";
 
 import PostHeader from "./PostHeader";
-
-const DUMMY_POST = {
-  title: "POST Title which is very long so that I can test something",
-  excerpt: "this is blog post excerpt",
-  imagePath:
-    "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1120&q=80",
-  content: "## This is post content. hello world",
-  date: "2022-10-06",
-};
 
 interface PostContentProps {
   post: Post;
@@ -39,15 +28,6 @@ const customRenderers = {
   },
 };
 
-// const CodeBlock = ({ className, children }: CodeBlockProps) => {
-//   const className.split()
-//   return (
-//     <div>
-//       <SyntaxHighlighter language={language} style={atomDark} children={children}>
-//     </div>
-//   );
-// };
-
 const PostContent = (props: PostContentProps) => {
   const { post, content } = props;
   const postDate = new Date(post.createdAt);
@@ -55,13 +35,12 @@ const PostContent = (props: PostContentProps) => {
   return (
     <>
       <div className="w-[90%] m-auto mt-[5%] lg:mt-[2%] lg:w-[60%] lg:max-w-[1000px]">
-        <PostHeader title={post.title} image={DUMMY_POST.imagePath} date={postDate} />
+        <PostHeader title={post.title} image={post.thumbnail} date={postDate} />
         <article className="prose max-w-none">
           <ReactMarkdown components={customRenderers} remarkPlugins={[remarkGfm]}>
             {content}
           </ReactMarkdown>
         </article>
-        {/* {DUMMY_POST.content} */}
       </div>
     </>
   );
