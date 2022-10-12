23d7f1ced071555a87e5e3389bc02a766fdbe7f8
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-10-12 17:47:53 +0900

    feat: code syntax highlight

--- pages/components/Blog/posts/post-detail/PostContent.tsx
+++ pages/components/Blog/posts/post-detail/PostContent.tsx
@@ -1,4 +1,8 @@
+import Image from "next/image";
+import { ReactNode } from "react";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { nightOwl } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import remarkGfm from "remark-gfm";
 import { Post } from "../../../../../interfaces/post.interface";
 
@@ -17,6 +21,33 @@ interface PostContentProps {
   post: Post;
   content: string;
 }
+
+const customRenderers = {
+  code(code: any) {
+    const { node, inline, className, children, ...props } = code;
+    const match = /language-(\w+)/.exec(className || "");
+
+    return !inline && match ? (
+      <SyntaxHighlighter style={nightOwl} language={match[1]}>
+        {children}
+      </SyntaxHighlighter>
+    ) : (
+      <code className={className} {...props}>
+        {children}
+      </code>
+    );
+  },
+};
+
+// const CodeBlock = ({ className, children }: CodeBlockProps) => {
+//   const className.split()
+//   return (
+//     <div>
+//       <SyntaxHighlighter language={language} style={atomDark} children={children}>
+//     </div>
+//   );
+// };
+
 const PostContent = (props: PostContentProps) => {
   const { post, content } = props;
   const postDate = new Date(post.createdAt);
@@ -26,7 +57,9 @@ const PostContent = (props: PostContentProps) => {
       <div className="w-[90%] m-auto mt-[5%] lg:mt-[2%] lg:w-[60%] lg:max-w-[1000px]">
         <PostHeader title={post.title} image={DUMMY_POST.imagePath} date={postDate} />
         <article className="prose max-w-none">
-          <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+          <ReactMarkdown components={customRenderers} remarkPlugins={[remarkGfm]}>
+            {content}
+          </ReactMarkdown>
         </article>
         {/* {DUMMY_POST.content} */}
       </div>
