733d1103d15111fb40873ef35da8297ef337097e
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-10-15 02:30:19 +0900

    chore: 미완성 페이지에 메세지 표시

--- pages/frontend/index.tsx
+++ pages/frontend/index.tsx
@@ -1,6 +1,11 @@
 import { NextPage } from "next";
 
 const FrontendPage: NextPage = () => {
-  return <div>FrontendPage</div>;
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen">
+      <div className="text-4xl">Frontend Page</div>
+      <div className="text-2xl">Under Maintenance</div>
+    </div>
+  );
 };
 export default FrontendPage;
--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,6 +1,11 @@
 import { NextPage } from "next";
 
 const PortfolioPage: NextPage = () => {
-  return <div>PortfolioPage</div>;
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen">
+      <div className="text-4xl">Portfolio Page</div>
+      <div className="text-2xl">Under Maintenance</div>
+    </div>
+  );
 };
 export default PortfolioPage;
