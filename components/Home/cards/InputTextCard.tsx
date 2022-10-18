6f8deb51c77cf9c0ab2ba692a3b545ff507d025b
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-10-18 23:16:45 +0900

    feat: Questions 생성

--- components/Home/cards/InputTextCard.tsx
+++ components/Home/cards/InputTextCard.tsx
@@ -1,4 +1,10 @@
-const InputTextCard = (props: { title: string; placeholder: string }) => {
+import { LegacyRef } from "react";
+
+const InputTextCard = (props: {
+  title: string;
+  placeholder: string;
+  inputRef: LegacyRef<HTMLInputElement> | undefined;
+}) => {
   return (
     <div className="h-[80%] bg-input-box-color rounded pt-2 pb-0 px-4 hover:border-2 focus-within:border-2 focus-within:border-primary-orange hover:border-primary-orange md:rounded-xl">
       <p className="text-sm font-bold text-primary-orange md:text-lg">{props.title}</p>
@@ -6,6 +12,7 @@ const InputTextCard = (props: { title: string; placeholder: string }) => {
         type="text"
         placeholder={props.placeholder}
         className="bg-input-box-color text-lg font-semibold placeholder:text-placeholder-color pt-5 w-full outline-0 md:text-xl"
+        ref={props.inputRef}
       />
     </div>
   );
